2e4dcd8fc454b0090fe5b7f56ed57978f904a5f5
muhammad_shoaib <muhammad_shoaib@pitb.gov.pk>
2023-06-15 20:07:36 +0500

    test controller

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -10,9 +10,14 @@ class TestController extends Controller
     {
         echo "index function";
     }
-    
+
     public function create()
     {
         echo "create function";
+    }
+
+    public function store()
+    {
+        echo "store function";
     }
 }
